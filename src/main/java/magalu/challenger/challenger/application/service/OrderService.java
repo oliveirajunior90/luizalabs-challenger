7b276f0912a2ab92e46143f045eb4a2fc880f99e
almirjunior <almirjunior@digix.com.br>
2025-06-30 07:55:25 -0300

    chore: refactor order services

--- src/main/java/magalu/challenger/challenger/application/service/OrderService.java
+++ src/main/java/magalu/challenger/challenger/application/service/OrderService.java
@@ -1,70 +1,17 @@
 package magalu.challenger.challenger.application.service;
 
-import magalu.challenger.challenger.application.dto.OrderItemDTO;
 import magalu.challenger.challenger.application.dto.OrderWithUserDTO;
 import magalu.challenger.challenger.application.dto.PageResponseDTO;
-import magalu.challenger.challenger.application.dto.UserDTO;
-import magalu.challenger.challenger.domain.entity.Order;
-import magalu.challenger.challenger.domain.entity.OrderItem;
-import magalu.challenger.challenger.infraestructure.repository.OrderRepository;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
-@Service
-public class OrderService {
+public interface OrderService {
+    OrderWithUserDTO getOrderById(Long orderId);
 
-    OrderRepository orderRepository;
-
-    public OrderService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
-
-    public OrderWithUserDTO getOrderById(Long orderId) {
-        Order order = orderRepository.findOrderById(orderId);
-
-        return transformOrderToDTO(order);
-    }
-
-    public PageResponseDTO<OrderWithUserDTO> getOrdersByDateRange(
+    PageResponseDTO<OrderWithUserDTO> getOrdersByDateRange(
             LocalDate startDate,
             LocalDate endDate,
             Pageable pageable
-    ) {
-        Page<Order> orders = orderRepository.findByPurchaseDateBetween(startDate, endDate, pageable);
-
-        List<OrderWithUserDTO> ordersContent = orders
-                .getContent()
-                .stream()
-                .map(this::transformOrderToDTO).collect(Collectors.toList());
-
-        return new PageResponseDTO<>(
-            ordersContent,
-            orders.getNumber(),
-            orders.getSize(),
-            orders.getTotalElements(),
-            orders.getTotalPages()
-        );
-    }
-
-    private OrderWithUserDTO transformOrderToDTO(Order order) {
-        return new OrderWithUserDTO(
-                order.getId(),
-                order.getPurchaseDate().toString(),
-                order.getTotalPrice().toString(),
-                order.getItems().stream().map(this::transformOrderItemToDTO).collect(Collectors.toList()),
-                new UserDTO(order.getUser().getId(), order.getUser().getName())
-        );
-    }
-
-    private OrderItemDTO transformOrderItemToDTO(OrderItem orderItem) {
-        return new OrderItemDTO(
-                orderItem.getProductId(),
-                orderItem.getProductValue().toString()
-        );
-    }
+    );
 }
--- src/main/java/magalu/challenger/challenger/application/service/orderimport/OrderImportService.java
+++ src/main/java/magalu/challenger/challenger/application/service/orderimport/OrderImportService.java
@@ -1,66 +1,8 @@
 package magalu.challenger.challenger.application.service.orderimport;
 
-import jakarta.transaction.Transactional;
-import magalu.challenger.challenger.domain.entity.Order;
-import magalu.challenger.challenger.domain.entity.OrderItem;
-import magalu.challenger.challenger.domain.entity.User;
-import magalu.challenger.challenger.infraestructure.repository.OrderItemRepository;
-import magalu.challenger.challenger.infraestructure.repository.OrderRepository;
-import magalu.challenger.challenger.infraestructure.repository.UserRepository;
-import magalu.challenger.challenger.infraestructure.fileimport.parser.OrderFileParserDTO;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 
-@Service
-public class OrderImportService {
-
-    private final OrderFileParser parser;
-    private final UserRepository userRepository;
-    private final OrderRepository orderRepository;
-    private final OrderItemRepository itemRepository;
-
-    public OrderImportService(OrderFileParser parser, UserRepository userRepository, OrderRepository orderRepository, OrderItemRepository itemRepository) {
-        this.parser = parser;
-        this.userRepository = userRepository;
-        this.orderRepository = orderRepository;
-        this.itemRepository = itemRepository;
-    }
-
-    @Transactional
-    public void importFromFile(Path path) {
-        Map<Long, User> users = new HashMap<>();
-        Map<Long, Order> orders = new HashMap<>();
-        List<OrderItem> orderItems = new ArrayList<>();
-
-        try (Stream<String> lines = Files.lines(path)) {
-            lines.forEach(line -> {
-                OrderFileParserDTO dto = parser.parseLine(line);
-
-                users.putIfAbsent(dto.userId(), new User(dto.userId(), dto.userName()));
-                orders.putIfAbsent(
-                            dto.orderId(),
-                            new Order(dto.orderId(), users.get(dto.userId()), dto.purchaseDate())
-                );
-
-                OrderItem item = new OrderItem(dto.orderId(), dto.productId(), dto.productValue());
-                orderItems.add(item);
-            });
-
-            userRepository.saveAll(users.values());
-            orderRepository.saveAll(orders.values());
-            itemRepository.saveAll(orderItems);
-
-        } catch (IOException e) {
-            throw new UncheckedIOException("Erro ao ler arquivo", e);
-        }
-    }
+public interface OrderImportService {
+    void importFromFile(Path path) throws UncheckedIOException;
 }
